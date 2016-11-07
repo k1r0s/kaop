c0937f248cbcaf5daeb9bbc70e8838b45b4a23cc
ciroreed <ciroreed@gmail.com>
2016-11-08 00:22:56 +0100

    fix module require get undefined in browserify

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var Class = require("./src/Class");
 var annotations = require("./src/annotations");
 
-if (typeof window === "object") {
-  window.Class = Class;
-  window.annotations = annotations;
-} else {
+if (typeof module === "object") {
   module.exports = {
     Class: Class,
     annotations: annotations
   };
+} else {
+  window.Class = Class;
+  window.annotations = annotations;
 }
