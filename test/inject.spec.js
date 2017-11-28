4d67c908701bbae1bd4e31615fd898d3aa35bd7f
k1r0s <ciro@mint-labs.com>
2017-11-28 10:32:10 +0100

    added dependency tests

--- test/inject.spec.js
+++ test/inject.spec.js
@@ -3,66 +3,54 @@ var inject = require('../inject');
 var provide = require('../provider');
 var override = require('../override');
 
-var Observable = provide.factory(type({
+var EventEmitter, EventEmitterProvider = provide.factory(EventEmitter = type({
   actions: [],
-  subscribe: function(uid, handler) {
+  when: function(uid, handler) {
     this.actions.push({ uid, handler });
   },
-  trigger: function(uid, data) {
+  force: function(uid, data) {
     var matched = this.actions.find(action => action.uid === uid)
 
-    matched && matched.call(undefined, data)
+    matched && matched.handler.call(undefined, data);
   },
-  unsubscribe: function(uid) {
+  ignore: function(uid) {
     this.actions = this.actions.filter(action => action.uid !== idEvent);
   }
-}))
+}));
 
-var RemoteCollection = type.inherits(Array, {
+var ObservableArray = type.inherits(Array, {
   $obs: null,
-  constructor: [inject.param(Observable), (obs) => {
+  constructor: [inject.param(EventEmitterProvider), function(obs) {
     this.$obs = obs;
   }],
-  push: [override.apply, () => {
-    this.$obs.trigger("update");
+  onChange: function(cbk) {
+    this.$obs.when("update", cbk.bind(this));
+  },
+  push: [override.apply, function() {
+    this.$obs.force("update");
   }],
-  pop: [override.apply, () => {
-    this.$obs.trigger("update");
-  }],
-  unshift: [override.apply, () => {
-    this.$obs.trigger("update");
-  }],
-  shift: [override.implement, () => {
-    this.$obs.trigger("update");
+  pop: [override.apply, function() {
+    this.$obs.force("update");
   }]
 });
 
-var list1, list2;
+var list;
 
-describe("advice specs", () => {
+describe("inject specs", () => {
   beforeEach(() => {
-    list1 =  new Array;
-    list2 =  new CustomList;
+    list = new ObservableArray;
   });
 
-  it("should be able to call custom actions without messing proto behavior", () => {
-    list2.push(3, 4);
-    list1.push(3, 4);
-    expect(list1[1]).toEqual(list2[1]);
-    list2.pop();
-    list1.pop();
-    expect(list1[1]).toEqual(list2[1]);
+  it("should be able to inject dependencies", () => {
+    expect(list.$obs).toBeInstanceOf(EventEmitter);
   });
 
-  it("extended type should be able keep original behavior featuring new things", () => {
-    list2.push(3, 4);
-    list2.unshift(9);
-    list2.push(5, 4, 7, 7);
-    expect(list2.total).toEqual(39);
-    list2.pop();
-    list2.shift();
-    list2.push(3);
-    list2.push(9);
-    expect(list2.total).toEqual(35);
+  it("injected instance should have full capabilities", done => {
+    list.onChange(function(){
+      expect(this[0]).toEqual(42);
+      done();
+    });
+
+    list.push(42);
   });
 });
