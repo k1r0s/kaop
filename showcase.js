933355b75596cec2b4a462d2abb7ce2441aee2a3
k1r0s <ciroreed@gmail.com>
2017-12-02 14:09:21 +0100

    improve a bit showcase capabilities

--- showcase.js
+++ showcase.js
@@ -1,9 +1,13 @@
-const { createClass, reflect } = require('.');
+const { createClass, reflect, provider, inject } = require('.');
 
+// Log advice. This advice logs all info within the called method
 const Log = reflect.advice(function(meta){
-  console.info(`${meta.key} method called with ${JSON.stringify(meta.args)} returned ${meta.result}`);
+  console.info(`${meta.key} method called with
+    ${JSON.stringify(meta.args)} returned ${meta.result}`);
 });
 
+// ChargeBean advice. if the first argument is truthy, next
+// function will be delayed 100ms
 const ChargeBean = reflect.advice(function(meta){
   if(!meta.args[0]) {
     meta.commit();
@@ -13,6 +17,14 @@ const ChargeBean = reflect.advice(function(meta){
   }
 });
 
+// create a provider of Beam class
+const BeamProvider = provider.factory(createClass({
+  use(beamType) {
+    return `firing ${beamType}`;
+  }
+}));
+
+// LoggerAspect applies Log advice to every method
 const LoggerAspect = reflect.aspect((currentProps, key) => {
   if(currentProps[key] instanceof Function) {
     currentProps[key] = [currentProps[key], Log];
@@ -22,6 +34,7 @@ const LoggerAspect = reflect.aspect((currentProps, key) => {
   return currentProps;
 });
 
+// SomersaultAspect adds `doSommersault` method
 const SomersaultAspect = reflect.aspect((currentProps, key) => {
   if(typeof currentProps.doSommersault !== "function") {
     currentProps.doSommersault = function() {
@@ -32,22 +45,28 @@ const SomersaultAspect = reflect.aspect((currentProps, key) => {
   return currentProps;
 })
 
-const Human = createClass(LoggerAspect(SomersaultAspect({
-  constructor(name){
-    this.name = name;
-  },
-  beam: [ChargeBean, function(charging, beamType) {
-    return `firing${charging ? " charged": ""} ${beamType}!`;
-  }],
-  sayHello() {
-    return `Hey, I'm ${this.name}`;
-  }
-})));
+// create an Human class with LoggerAspect and SomersaultAspect
+// and we apply ChargeBean advice directly to `fire` method
+const Human = createClass(
+  LoggerAspect(SomersaultAspect({
+    // inject beam instance on $beam property before
+    // main constructor gets executed
+    constructor: [inject.assign({ $beam: BeamProvider }), function(_name){
+      this.name = _name;
+    }],
+    fire: [ChargeBean, function(charging, fireType) {
+      return this.$beam.use(fireType);
+    }],
+    sayHello() {
+      return `Hey, I'm ${this.name}`;
+    }
+  }))
+);
 
 const human = new Human("Samus Aran");
 
 human.sayHello();
 human.doSommersault();
-human.beam(false, "normal beam");
-human.beam(false, "ice beam");
-human.beam(true, "plasma beam");
+human.fire(false, "normal");
+human.fire(false, "ice");
+human.fire(true, "plasma");
