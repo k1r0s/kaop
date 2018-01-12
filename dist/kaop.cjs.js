7542b0afd6c40e28dbba01ac8419de185023ce82
k1r0s <ciroreed@gmail.com>
2018-01-12 23:08:07 +0100

    publish missing files

--- dist/kaop.cjs.js
+++ dist/kaop.cjs.js
@@ -165,7 +165,7 @@ function args(){
   var providers = Array.prototype.slice.call(arguments);
   return reflect.advice(function(meta) {
     if (meta.key !== "constructor") { throw new Error("inject only available in constructor") }
-    meta.args = providers.map(function(provider) { return provider() });
+    providers.forEach(function(provider) { meta.args.push(provider()); });
   })
 }
 
